dfdfe78badd54768817cf1c7acf5d3865536717f
GgorgeousS <104258737+GgorgeousS@users.noreply.github.com>
2023-02-08 21:36:43 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,97 @@
+let albums = [
+    {
+        title: `Собраться с мыслями`,
+        img: `assets/1.jpg`,
+        year: `2023`,
+        description: `Лёгкая неоклассика помогут сконцентрироваться на рабочих задачах.`,
+        tracks: [ 
+            {
+                title:`Fantasia in D Minor, KV 397`,
+                author:`Вольфганг Амадей Моцарт`,
+                time:`5:34`
+            },
+            {
+                title:`Serenade in G Major, K. 525 "Eine kleine Nachtmusik": II. Romance: Andante`,
+                author:`Вольфганг Амадей Моцарт`,
+                time:`6:44`
+            },
+            {
+                title:`Piano Concerto No. 23 in A Major, K. 448: II. Andante`,
+                author:`Вольфганг Амадей Моцарт`,
+                time:`6:44`
+            }
+
+        ]
+    },
+    {
+        title: `Прогулка под дождем`,
+        img: `assets/2.jpg`,
+        year: `2021`,
+        description: `Лёгкая неоклассика помогут сконцентрироваться на рабочих задачах.`,
+        tracks: [ 
+            {
+                title:`Children's Album, Op. 39: IV. Little Mother`,
+                author:`Пётр Ильич Чайковский`,
+                time:`1:57`
+            },
+            {
+                title:`Children's Album, Op. 39: VIII. Waltz`,
+                author:`Пётр Ильич Чайковский`,
+                time:`1:31`
+            },
+            {
+                title:`Concerto No. 1 in B-Flat Minor for Piano and Orchestra, Op. 23: II. Andantino simplice`,
+                author:`Пётр Ильич Чайковский`,
+                time:`6:44`
+            }
+        ]        
+    },
+    {
+        title: `Для размышлений`,
+        img: `assets/3.jpg`,
+        year: `2013`,
+        description: `Лёгкая неоклассика помогут сконцентрироваться на рабочих задачах.`,
+        tracks: [ 
+            {
+                title:`4 Romances, Op.2: II. Eastern Song (The Nightingale Enslaved by the Rose)`,
+                author:`Николай Андреевич Римский-Корсаков`,
+                time:`2:45`
+            },
+            {
+                title:`Piano Sonata No. 14 in C-Sharp Minor, Op. 27 No. 2 "Moonlight": I. Adagio sostenuto`,
+                author:`Людвиг ван Бетховен`,
+                time:`5:28`
+            },
+            {
+                title:`The Four Seasons (Le Quattro Stagioni), Op. 8, Concerto No. 4 in F Minor, RV 297 "Winter": I. Allegro non molto (Arr. for 2 Violins and Piano)`,
+                author:`Антонио Вивальди`,
+                time:`3:29`
+            }
+
+        ]
+    },
+    {
+        title: `Для чтения`,
+        img: `assets/4.jpg`,
+        year: `2024`,
+        description: `Лёгкая неоклассика помогут сконцентрироваться на рабочих задачах.`,
+        tracks: [ 
+            {
+                title:`Piano Sonata No. 1 in D Minor, Op. 28: II. Lento`,
+                author:`Сергей Васильевич Рахманинов`,
+                time:`10:24`
+            },
+            {
+                title:`Arpeggione Sonata in A minor, D. 821: I. Allegro moderato`,
+                author:`Франц Шуберт`,
+                time:`12:19`
+            },
+            {
+                title:`Children's Album, Op. 39: XXIV. In The Church`,
+                author:`Пётр Ильич Чайковский`,
+                time:`2:47`
+            }
+
+        ]
+    },
+]
